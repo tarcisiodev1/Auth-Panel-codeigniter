142c2c32ceeba4803e0895182d271460cbd534cb
Tarcisio Santos Junior <tarcisiodev1@gmail.com>
2023-07-12 00:33:35 -0300

    migrate and model

---
+++ app/Database/Migrations/2023-07-12-031209_Post.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+use CodeIgniter\Database\RawSql;
+
+class Post extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'constraint' => 5,
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+            'user_id' => [
+                'type' => 'INT',
+                'unsigned' => true,
+                'null' => false,
+            ],
+
+            'title' => [
+                'type' => 'VARCHAR',
+                'null' => false,
+                'constraint' => 255,
+            ],
+            'slug' => [
+                'type' => 'VARCHAR',
+                'null' => false,
+                'constraint' => 255,
+            ],
+            'description' => [
+                'type' => 'TEXT',
+                'null' => false,
+            ],
+            'created_at' => [
+                'type' => 'TIMESTAMP',
+                'default' => new RawSql('CURRENT_TIMESTAMP'),
+            ],
+            'updated_at' => [
+                'type' => 'TIMESTAMP',
+                'default' => new RawSql('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'),
+            ],
+        ]);
+
+        $this->forge->addPrimaryKey('id');
+        $this->forge->addForeignKey('user_id', 'users', 'id', '', 'CASCADE');
+        $this->forge->createTable('posts');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('posts');
+    }
+}
