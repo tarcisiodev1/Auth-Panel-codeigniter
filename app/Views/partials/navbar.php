7f9bd48ec83e4d978ae242c3e5b1189a985ee1e7
Tarcisio Santos Junior <tarcisiodev1@gmail.com>
2023-07-31 10:46:17 -0300

    nav

--- app/Views/partials/navbar.php
+++ app/Views/partials/navbar.php
@@ -11,10 +11,12 @@
     <div class="collapse navbar-collapse justify-content-between" id="navbarNav">
         <ul class="navbar-nav">
             <li class="nav-item active">
-                <a class="nav-link text-white" href="<?= url_to('/') ?>"><img src="public\assets\img\nerd (1).png" alt=""></a>
+                <a class="nav-link text-white" href="<?= url_to('/') ?>"><img src="public\assets\img\nerd (1).png"
+                        alt=""></a>
             </li>
             <li class="nav-item active">
-                <a class="nav-link text-white" href="<?= url_to('/') ?>"><img src="public\assets\img\nerd (1).png" alt="">Home</a>
+                <a class="nav-link text-white" href="<?= url_to('/') ?>"><img src="public\assets\img\nerd (1).png"
+                        alt="">Home</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link text-white" href="<?= url_to('auth.login') ?>">Login</a>
@@ -24,14 +26,19 @@
             </li>
         </ul>
         <?php if (session()->has('user')) : ?>
-            <ul class="navbar-nav">
-                <li class="nav-item ">
-                    <a class="nav-link text-white" href="<?= url_to('post.store') ?>">+</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-white" href="<?= url_to('dashboard') ?>"><?= session()->get('user') ?></a>
-                </li>
-            </ul>
+        <ul class="navbar-nav">
+            <li class="nav-item ">
+                <a class="nav-link text-white" href="<?= url_to('post.store') ?>"><svg
+                        xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-plus"
+                        viewBox="0 0 16 16">
+                        <path
+                            d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
+                    </svg></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-white" href="<?= url_to('dashboard') ?>"><?= session()->get('user') ?></a>
+            </li>
+        </ul>
         <?php endif; ?>
     </div>
 </nav>
