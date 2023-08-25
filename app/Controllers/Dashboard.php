3eff8658ee5dde1409e80d81f382ccc0326c23f3
Tarcisio Santos Junior <tarcisiodev1@gmail.com>
2023-08-25 00:32:31 -0300

    validação

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -32,7 +32,7 @@ class Dashboard extends BaseController
                     'is_image[userfile]',
                     // 'mime_in[userfile,image/jpg,image/jpeg,image/gif,image/png,image/webp]',
                     // 'max_size[userfile,100]',
-                    // 'max_dims[userfile,1024,768]',
+
                 ],
             ],
         ];
